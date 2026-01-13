d80a610724d31dcf31e2ecced8844803a47f6c59
masenjo <masenjo@portatilAcer.com>
2026-01-13 20:22:54 +0100

    fix(builderrors): Fixed building and deploying, but now it shows a black screen, TODO

--- app/src/androidTest/java/com/masenjo_android/asociacionmayoresvillanueva/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/masenjo_android/asociacionmayoresvillanueva/ExampleInstrumentedTest.kt
@@ -1,24 +1,20 @@
 package com.masenjo_android.asociacionmayoresvillanueva
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import com.masenjo_android.asociacionmayoresvillanueva.ui.activity.MainActivity
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.masenjo_android.asociacionmayoresvillanueva", appContext.packageName)
-    }
+
+  @get:Rule
+  val rule = ActivityScenarioRule(MainActivity::class.java)
+
+  @Test
+  fun mainActivity_launches() {
+    // Si llega aquí sin crashear, el test pasa.
+  }
 }
